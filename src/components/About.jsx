442e219cd56882480937b6f425e1931a5b0c9d92
Suman <iamsuman898@gmail.com>
2022-09-05 23:26:16 +0530

    add : about, fix:resp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,13 +3,15 @@ import '../styles/About.css';
 const About =()=>{
     return(
         <>
-            <main className="about-wrapper">
+            <main className="about-wrapper" id="about">
                 <div className="about-title">
                     <h2>Who am I ?</h2>
-                    <p> Here is a brief intro about me 📌</p>
+                    <div className="underline"></div>
+                    <p>A brief bio📌</p>
                 </div>
                 <div className="about-description">
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe enim, quas, voluptates reiciendis eum temporibus qui aut maxime provident quae magnam maiores quod, id deleniti? Eveniet fuga officia illum beatae.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis eius delectus nisi iste beatae enim vitae sit? Doloremque illum laborum maxime earum cum a, obcaecati dolorem dolor quis repellat quas!</p>
                 </div>
             </main>
         </>
