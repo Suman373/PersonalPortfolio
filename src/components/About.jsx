76c76eb8f736d90c7915bf387691b9c451b5c53c
Suman <iamsuman898@gmail.com>
2022-09-06 19:39:54 +0530

    achievements added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,16 @@ const About =()=>{
                     <p>A brief bio📌</p>
                 </div>
                 <div className="about-description">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe enim, quas, voluptates reiciendis eum temporibus qui aut maxime provident quae magnam maiores quod, id deleniti? Eveniet fuga officia illum beatae.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis eius delectus nisi iste beatae enim vitae sit? Doloremque illum laborum maxime earum cum a, obcaecati dolorem dolor quis repellat quas!</p>
+                    <p>My name is Suman Roy. I am currently pursuing my bachelors in computer science. I have keen interest and a passion for the web, so I started learning web development few months ago. With time I kept on honing my skills, moved onto frameworks and other tools. </p>
+                    <p>I have always fancied hackathons. It is a great way of letting like minded people from different aspects and domains like dev, designer ,manager, speaker,etc come together to brainstorm ideas and turn them into reality. I have participated in few online hackathons, made it to top teams in one.</p>
+                    <p>Open source is something I like. A developer can excel if they start contributing to open source by any means. I have contributed to few projects on github too!</p>
+
+                    <h3>My achievements</h3>
+                    <div className="underline"></div>
+                    <ul>
+                        <li>Mentee at DSC KGEC Summer of Code 2022. Merged 2 successful PRs on a web project.</li>
+                        <li>Top 14 among 65+ teams in Error 404 hackathon organized by Micro. Me and my teammate made a project in the track of web development.</li>
+                    </ul>
                 </div>
             </main>
         </>
