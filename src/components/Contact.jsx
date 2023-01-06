90edb442779d8e09d99545aa8ea9e6975d91818a
Suman Roy <95040233+Suman373@users.noreply.github.com>
2023-01-06 16:07:56 +0530

    feat: realtime email service, contact form
    Integrated emailjs for sending form data to my email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import '../styles/Contact.css';
 import {useNavigate} from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useInView } from "react-intersection-observer";
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    // ref
+    const ref = useRef();
     // submission
-    const handleSubmit = (e) => {
+    const handleSubmit = (e) =>{
         e.preventDefault();
-        toast('Submited successfullyy', {theme:'colored',type:'success'});
+        emailjs.sendForm(`${process.env.REACT_APP_EJS_SERVICE_ID}`
+        ,`${process.env.REACT_APP_EJS_TEMPLATE_ID}`,
+        ref.current,
+        `${process.env.REACT_APP_EJS_USER_ID}`).then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          toast('Submitted successfully', {theme:'colored',type:'success'});
+       }, function(error) {
+          console.log('FAILED...', error);
+          toast('Something went wrong', {theme:'colored', type:'error'});
+       });
+       ref.current.reset();
     }
 
     const {ref: myTitleRef , inView : titleVisible} = useInView();
@@ -20,22 +33,11 @@ const Contact = () => {
                 Get in <span className="heading-highlight">touch</span> with me 💬
             </h2>
             <div className="underline"></div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">
-                    Name
-                </label>
-                <input type="text" placeholder="Your name" required />
-
-                <label htmlFor="email">
-                    Email
-                </label>
-                <input type="email" placeholder="Your email" required/>
-
-                <label htmlFor="textarea">
-                    Text
-                </label>
-                <textarea id="textarea" placeholder="Something you would like to say..." required></textarea>
-
+            <p>Fill up this form with correct details to connect with me</p>
+            <form ref={ref} onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder="Your name" required />
+                <input type="email" name="email" placeholder="Your email" required/>
+                <textarea id="textarea" name="message" placeholder="Your message" required></textarea>
                 <button
                     className="submit-btn"
                     type="submit"
